3835c8246213073b3b944d235a80b3f5e33b43d6
Cata <cata@Apples-MacBook-Air.local>
2021-12-07 21:02:10 +0200

    dasd

--- models/project.js
+++ models/project.js
@@ -1,15 +1,13 @@
 const sequelize = require("../sequelize");
 const DataTypes = require("sequelize/dist");
 const Project = sequelize.define("Project", {
-  className: { type: DataTypes.STRING },
-  studentId: {
-    type: DataTypes.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-  },
   projectId: {
     type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
   },
+  projectName:{
+     type: DataTypes.STRING,
+     allowNull: false 
+  }
 });
