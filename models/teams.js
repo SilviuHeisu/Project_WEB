6ae7616f6905ef75e6d9427bbeed6cd982dd7211
Silviu Heisu <heisusilviu19@stud.ase.ro>
2021-12-14 17:32:02 +0200

    1

--- models/teams.js
+++ models/teams.js
@@ -2,12 +2,11 @@ const sequelize = require("../sequelize");
 const { DataTypes } = require("sequelize/dist");
 
 const Teams = sequelize.define("Teams", {
-    teamId: {
-        type: DataTypes.INTEGER,
+  teamId: {
+    type: DataTypes.INTEGER,
     primaryKey: true,
     autoIncrement: true,
-    },
-    teamName: { type: DataTypes.STRING, allowNull: false }
-
+  },
+  teamName: { type: DataTypes.STRING, allowNull: true },
 });
 module.exports = Teams;
