f9fab2c0a8dc5bb1818d4340a3d493d7c26db13c
Cata <cata@Apples-MacBook-Air.local>
2021-12-26 18:39:30 +0200

    Catalin Home + LoginProfessor

--- project-app/src/components/Home.js
+++ project-app/src/components/Home.js
@@ -4,19 +4,46 @@ import * as ReactDOM from "react-dom";
 import axios from "axios";
 import { useState } from "react";
 import { useEffect } from "react";
+
+function TeamList(props){
+  const teams = props.teams;
+  for(let i=0; i<teams.length;i++)
+   { console.log(teams[i].teamId);
+    console.log(teams[i].teamName);
+   }
+   const listItems = teams.map((team) =>
+    <div><li>{team.teamId}</li>
+   <li>{team.teamName}</li>
+   <br></br></div>
+ );
+ return (
+   <ul>{listItems}</ul>
+ );}
+
 const Home = () => {
   const search = useLocation().search;
   const teamId = localStorage.getItem("teamId");
   const [teams, setTeams] = useState([]);
   const isLoggedIn = sessionStorage.getItem("isLoggedIn");
+  const user = sessionStorage.getItem("user");
+  const isProfessor = sessionStorage.getItem("isProfessor");
   useEffect(() => {
     axios.get("http://localhost:7000/team").then((response) => {
-      setTeams((teams) => [response.data]);
-      console.log(teams);
+      setTeams(response.data);
     });
-  }, []);
+    //setIsProfessor(sessionStorage.getItem("isProfessor"));
 
-  return <div>{isLoggedIn ? <div></div> : null}</div>;
+  },[]);
+
+  console.log(teams);
+  console.log(isLoggedIn);
+  console.log(user);
+  return <div>
+    {isLoggedIn ? (
+        <div> <h2>Hello there!</h2>
+        <TeamList teams={teams}/>
+           </div>): null}
+      </div>;
 };
 
 export default Home;
--- project-app/src/components/Login.js
+++ project-app/src/components/Login.js
@@ -4,42 +4,58 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import { Navigate } from "react-router-dom";
 
+
 function Login() {
   let navigate = useNavigate();
   const [students, setStudents] = useState([]);
+  const [professors, setProfessors] = useState([]);
   const [user, setUser] = useState({});
   let [isLoggedIn, setIsLoggedIn] = useState(false);
+  let [isProfessor, setIsProfessor] = useState(false);
   let password;
 
   useEffect(() => {
     axios.get("http://localhost:7000/student").then((response) => {
       setStudents(response.data);
     });
+    axios.get("http://localhost:7000/user").then((response) => {
+      setProfessors(response.data);
+    });
   }, []);
-  function UserGreeting(props) {
-    return <h1>Welcome back !"</h1>;
+  function handleChange(e){
+    if(e.target.value === "professor"){
+      setIsProfessor(true);
+    }
+    else{
+      setIsProfessor(false);
+    }
   }
   function handleSubmit(e) {
     e.preventDefault();
     console.log(students);
+
     password = bcrypt.hashSync(
-      e.target[1].value,
+      e.target[2].value,
       "$2a$10$CwTycUXWue0Thq9StjUM0u"
     );
     console.log(password);
 
     let ok = false;
-    for (let i = 0; i < students.length; i++) {
+    
+    if(!isProfessor)
+    {for (let i = 0; i < students.length; i++) {
       console.log(students[i]);
-      if (students[i].email === e.target[0].value) {
+      if (students[i].email === e.target[1].value) {
         ok = true;
         if (students[i].password === password) {
-          alert("Successfuly logged in");
+          alert(`Successfuly logged in as Student`);
           setUser(students[i]);
           setIsLoggedIn(true);
           navigate("/home", {});
           localStorage.setItem("teamId", students[i].teamId);
           sessionStorage.setItem("isLoggedIn", true);
+          sessionStorage.setItem("isProfessor", isProfessor);
+          sessionStorage.setItem("user", user.name);
         } else {
           alert("Wrong Password");
         }
@@ -49,15 +65,45 @@ function Login() {
       alert("Wrong Email Address");
     }
   }
+  else{
+    for (let i = 0; i < professors.length; i++) {
+      console.log(professors[i]);
+      if (professors[i].email === e.target[1].value) {
+        ok = true;
+        if (professors[i].password === password) {
+          alert(`Successfuly logged in as Professor`);
+          setUser(professors[i]);
+          setIsLoggedIn(true);
+          navigate("/home", {});
+          localStorage.setItem("teamId", professors[i].teamId);
+          sessionStorage.setItem("isLoggedIn", true);
+          sessionStorage.setItem("isProfessor", isProfessor);
+          sessionStorage.setItem("user", user);
+        } else {
+          alert("Wrong Password");
+        }
+      }
+    }
+    if (ok === false) {
+      alert("Wrong Email Address");
+    }
+  }
+  }
   return (
     <div className="login">
       <h4>Login_Cata</h4>
       <form onSubmit={handleSubmit}>
+      <lable>Choose a type:</lable>
+      <select id="type" onChange={handleChange}>
+        <option  value="student">Student</option>
+        <option  value="professor">Professor</option>
+      </select>
+      <br></br>
         <input type="text" placeholder="Email Address" />
         <input type="password" placeholder="Password" />
         <button type="submit">Login</button>
       </form>
-      {isLoggedIn ? (
+      {/* {isLoggedIn ? (
         <div>
           <h2>
             {" "}
@@ -67,7 +113,7 @@ function Login() {
           <h3> Year of Study: {user.yearOfStudy}</h3>
           <h3> Faculty: {user.faculty}</h3>
         </div>
-      ) : null}
+      ) : null} */}
     </div>
   );
 }
