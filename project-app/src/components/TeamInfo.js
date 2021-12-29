5fd9e699b0a95fccd5fd32929edaea812d2a0904
Cata <cata@Apples-MacBook-Air.local>
2021-12-29 11:40:07 +0200

    catalin v44

--- project-app/src/components/TeamInfo.js
+++ project-app/src/components/TeamInfo.js
@@ -5,34 +5,41 @@ import { useState } from "react";
 const TeamInfo = (props) => {
   const [students, setStudents] = useState([]);
   const [teams, setTeams] = useState([]);
-  const listStudents = students.map((student) => (
-    <p>Hello</p>
-    // <div className="Div2">
-    //   <tr>
-    //     <td>
-    //       <ul>{student.firstName}</ul>
-    //     </td>
-    //     <td>
-    //       {" "}
-    //       <ul>{student.lastName}</ul>
-    //     </td>
-    //     <td>
-    //       {" "}
-    //       <ul>{student.yearOfStudy}</ul>
-    //     </td>
-    //   </tr>
-
-    //   <br></br>
-    // </div>
-  ));
+  let listStudents;
+  const lista1 = [1,2];
   useEffect(() => {
+    let path = `http://localhost:7000/student/${props.teamId}`
     axios
-      .get("http://localhost:7000/student", { params: props.teamId })
+      .get(path)
       .then((response) => {
         setTeams(response.data);
+        setStudents(response.data);
         console.log(response.data);
       });
+      
   }, []);
+
+  listStudents = students.map((student) => (
+    <div className="Div2">
+      <tr>
+        <td>
+          <ul>{student.firstName}</ul>
+        </td>
+        <td>
+          {" "}
+          <ul>{student.lastName}</ul>
+        </td>
+        <td>
+          {" "}
+          <ul>{student.yearOfStudy}</ul>
+        </td>
+      </tr>
+
+      <br></br>
+    </div>));
+    console.log(listStudents);
+    
+ 
 
   return <div>{listStudents}</div>;
 };
